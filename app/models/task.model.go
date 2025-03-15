40193098c84b3f75f9d1fec9069b6b821c9591f6
Lunaticsatoshi <deysarkersudeep@gmail.com>
2025-03-15 14:59:16 +0530

    :bricks: chore: updating task model

--- app/models/task.model.go
+++ app/models/task.model.go
@@ -1,11 +1,15 @@
 package models
 
+import "time"
+
 type Task struct {
-	ID          uint   `gorm:"column:id;primary_key" json:"id"`
-	Title       string `gorm:"column:title" json:"title"`
-	Description string `gorm:"column:description" json:"description"`
-	Status      string `gorm:"column:status" json:"status"`
-	UserId      uint   `gorm:"column:user_id" json:"user_id"`
+	ID          uint      `gorm:"column:id;primary_key" json:"id"`
+	Title       string    `gorm:"column:title" json:"title"`
+	Description string    `gorm:"column:description" json:"description"`
+	Status      string    `gorm:"column:status" json:"status"`
+	UserId      uint      `gorm:"column:user_id" json:"user_id"`
+	CreatedAt   time.Time `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt   time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
 
 func (Task) TableName() string {
